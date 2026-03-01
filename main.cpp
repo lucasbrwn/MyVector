642c0f79f8118f5587e444daf018ac4a9cac6d48
Lucas Brown <lucasbrown612@gmail.com>
2026-03-01 12:17:33 -0800

    Add demo file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "include/MyVector.hpp"
+
+int main() {
+    CPSC131::MyVector::MyVector<int> v;
+
+    for (int i = 1; i <= 5; i++) {
+        v.push_back(i);
+    }
+
+    v.insert(2, 99);
+    v.erase(1);
+
+    v.print_helper();
+}
